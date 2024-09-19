04fb9ef96e9b1f82316ef3e794fa1258f81422ae
Spirit-Tiger <55990867+Spirit-Tiger@users.noreply.github.com>
2024-09-19 20:05:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function gamificationLogic() {
 
-  const serverUrl = "https://promodotestserver.onrender.com";
+  const serverUrl = "https://promodo-test-1.vercel.app/";
 
   let expirationInDays;
 
